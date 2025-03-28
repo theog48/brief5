e400ef4a2715de6d8bdf06ff8c500acf0b28ddf6
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-03-28 19:24:38 +0100

    create template politique
    creation du template politique

---
+++ themes/chuckNorris/templatepagecontenue.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Template politique */
+get_header();
+?>
+<!-- Ajout de la page de politique de confidentialité --> 
+<div class="container my-5">
+    <h1 class="text-center mb-4"><?php the_title(); ?></h1>
+    <div class="content">
+        <?php the_content(); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
